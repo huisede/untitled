2917fbf90403d4191a8fd11bbef9d39df2d5f3db
huisede <yytvo163@163.com>
2017-12-18 20:19:37 +0800

    Ploy plot

--- UImain.py
+++ UImain.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from test_ui import Ui_VI_Accessment_System
 import sys
-from SG_ReadFile import *
+from Generate_Figs import *
 
 
 class MainUiWindow(QMainWindow, Ui_VI_Accessment_System):
@@ -24,15 +24,15 @@ class MainUiWindow(QMainWindow, Ui_VI_Accessment_System):
         self.actionDataEdit.triggered.connect(lambda: self.change_page(1))
         self.actionRatingDetails.triggered.connect(lambda: self.change_page(2))
         self.treeWidget.clicked.connect(self.select_tree_nodes)
-        self.pushButton_3.clicked.connect(self.add_combo_box)
+        self.pushButton_3.clicked.connect(self.button_clicked)  ###
 
     def change_page(self, index_page):
-            self.stackedWidget.setCurrentIndex(index_page)
+        self.stackedWidget.setCurrentIndex(index_page)
 
     def load_sysgain_data(self):
         filepath = QFileDialog.getOpenFileName(self, filter='*.csv')
         filepath_full = filepath[0]
-        self.MainProcess_thread = ThreadProcess(filepath_full)
+        self.MainProcess_thread = ThreadProcess(method='sg_cal_thread', filepath=filepath_full)
         self.MainProcess_thread.Message_Finish.connect(self.show_ax_pictures)
         self.MainProcess_thread.start()
 
@@ -59,30 +59,60 @@ class MainUiWindow(QMainWindow, Ui_VI_Accessment_System):
         combo_box = QComboBox(self)
         self.verticalLayout_2.addWidget(combo_box)
 
+    def button_clicked(self):
+        self.MainProcess_thread = ThreadProcess(method='radar_cal_thread')
+        self.MainProcess_thread.Message_Finish_2.connect(self.show_radar_pictures)
+        self.MainProcess_thread.start()
 
+    def show_radar_pictures(self, str):
+        dr2 = MyFigureCanvas(width=10, height=5, plot_type='2d-poly',
+                            data=self.MainProcess_thread.ax_holder_radar.theta,
+                            para1=self.MainProcess_thread.ax_holder_radar.data)
+        dr2.plot_radar_map_()
+        self.scene = QtWidgets.QGraphicsScene()
+        self.scene.addWidget(dr2)
+        # self.PicToolBar = NavigationBar(dr, self)  # 初始化PicToolBar（本质为Wedgit），绑定到dr这个FigureCanvas上，然后将Toolbar绑到Layout上
+        # self.gridLayout_5.addWidget(self.PicToolBar)
+        self.graphicsView.setScene(self.scene)
+        self.graphicsView.show()
 
 
 class ThreadProcess(QtCore.QThread):
 
     Message_Finish = QtCore.pyqtSignal(str)
+    Message_Finish_2 = QtCore.pyqtSignal(str)
 
-    def __init__(self, filepath):
+    def __init__(self, method, **kwargs):
         super(ThreadProcess, self).__init__()
-        self.file_path = filepath
+        self.method = method
+        self.kwargs = kwargs
 
     def run(self):
-        self.ax_holder_SG=main_(self.file_path)
+        try:
+            getattr(self, self.method, 'nothing')()
+        except BaseException as e:
+            print(e)
+
+    def sg_cal_thread(self):
+        self.ax_holder_SG = sg_main(file_path=self.kwargs['filepath'])
         self.Message_Finish.emit("计算完成！")
+
+    def radar_cal_thread(self):
+        try:
+            self.ax_holder_radar = radar_plot()
+            self.Message_Finish_2.emit('finish')
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
+    # ---------------QSS导入-------------------
     file = QtCore.QFile('css.qss')
     file.open(QtCore.QFile.ReadOnly)
     stylesheet = file.readAll()
-    QtWidgets.QApplication.setStyleSheet(app, stylesheet.data().decode('utf-8'))
-
+    QtWidgets.QApplication.setStyleSheet(app, stylesheet.data().decode('utf-8'))  # utf-8  byte编码为String
+    # ---------------实例化---------------------
     dlg = MainUiWindow()
     dlg.show()
     sys.exit(app.exec())
